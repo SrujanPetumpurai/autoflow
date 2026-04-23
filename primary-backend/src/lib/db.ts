b823c0d504b75d84d0e244326bccdbeddf54657b
P.Srujan <srujan.petumpurai@gmail.com>
2026-04-24 00:24:15 +0530

    lib in primary-backend

--- primary-backend/src/lib/db.ts
+++ primary-backend/src/lib/db.ts
@@ -1,4 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
-console.log("DATABASE_URL:", process.env.DATABASE_URL ? "SET" : "MISSING ❌");
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
